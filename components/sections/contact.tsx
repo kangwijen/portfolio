7d888045b172f36f5ce273b55e21ef1774fca6ae
kangwijen <110406976+kangwijen@users.noreply.github.com>
2024-12-20 15:20:16 +0700

    Update contact.tsx

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -36,7 +36,7 @@ export function Contact() {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <h2
           className="text-3xl font-bold text-center mb-8 enhanced-glitch"
-          data-text="contact"
+          data-text="Contact"
         >
           Contact
         </h2>
