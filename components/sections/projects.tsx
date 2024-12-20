b5de6ebd54797dbd849ad8163d8606dd806ff028
kangwijen <110406976+kangwijen@users.noreply.github.com>
2024-12-20 15:16:30 +0700

    Update projects.tsx

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -44,7 +44,7 @@ export function Projects() {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <h2
           className="text-3xl font-bold text-center mb-8 enhanced-glitch"
-          data-text="projects"
+          data-text="Projects"
         >
           Projects
         </h2>
